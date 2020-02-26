0e042a2627e181d920fac39b045d5f97adbde12e
Emmanuel Geoffray <emmanuel@soixantecircuits.fr>
2020-02-26 12:04:31 +0100

    initial import

---
+++ src/authentication.js
@@ -0,0 +1,25 @@
+const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication')
+const { LocalStrategy } = require('@feathersjs/authentication-local')
+const { expressOauth, OAuthStrategy } = require('@feathersjs/authentication-oauth')
+
+class Auth0Strategy extends OAuthStrategy {
+  async getEntityData (profile) {
+    const baseData = await super.getEntityData(profile)
+
+    return {
+      ...baseData,
+      ...profile
+    }
+  }
+}
+
+module.exports = app => {
+  const authentication = new AuthenticationService(app)
+
+  authentication.register('jwt', new JWTStrategy())
+  authentication.register('local', new LocalStrategy())
+  authentication.register('auth0', new Auth0Strategy())
+
+  app.use('/authentication', authentication)
+  app.configure(expressOauth())
+}
